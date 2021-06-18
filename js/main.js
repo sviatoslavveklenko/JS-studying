94b38fe0530b59c558593c2da08acb2aea353b08
Sviatoslav Veklenko <sviatoslavveklenko@gmail.com>
2021-06-18 15:50:33 +0300

    First exercise JS-studying

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-let man = {
-	name: "Jack",
-	surname: "Johnson",
-	age: 17,
-	isMarried: false
+'use strict';
+let money = prompt("Ваш бюджет на месяц?"),
+	time = prompt("Введите дату в формате YYYY-MM-DD");
+console.log("Бюджет на месяц составляет - " + money);
+console.log("Дата заполнения - " + time);
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	opitionExpences: {},
+	income: [],
+	savings: false,
 };
+let question_1 = prompt("Введите обязательную статью расходов в этом месяце"),
+	question_2 = prompt("Во сколько обойдется ?"),
+	question_3 = prompt("Введите обязательную статью расходов в этом месяце"),
+	question_4 = prompt("Во сколько обойдется ?");
+appData.expenses.question_1 = question_2;
+appData.expenses.question_3 = question_4;
 
-console.log(man.name, man.surname, man.age + ' years old.');
+
+console.log(appData);
+let moneyPerDay = money / 30;
+alert("Ваш бюджет на 1 день = " + moneyPerDay);
